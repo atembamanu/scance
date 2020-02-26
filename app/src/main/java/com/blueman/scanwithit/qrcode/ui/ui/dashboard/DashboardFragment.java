c034507e5e7ae671c78c7cd23645de0903f289ab
atembamanu <atembaemm07@gmail.com>
2020-02-26 21:29:51 +0300

    add calender and piechart

--- app/src/main/java/com/blueman/scanwithit/qrcode/ui/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/blueman/scanwithit/qrcode/ui/ui/dashboard/DashboardFragment.java
@@ -1,23 +1,36 @@
 package com.blueman.scanwithit.qrcode.ui.ui.dashboard;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.blueman.scanwithit.R;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 
-public class DashboardFragment extends Fragment {
+public class DashboardFragment extends Fragment implements OnChartValueSelectedListener {
 
     private DashboardViewModel dashboardViewModel;
+    private CalendarView calendarView;
+    private PieChart pieChart;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,12 +38,63 @@ public class DashboardFragment extends Fragment {
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getActivity(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        calendarView = root.findViewById(R.id.calendarView);
+        pieChart = root.findViewById(R.id.piechart);
+        dashboardViewModel.getText().observe(getActivity(), s -> textView.setText(s));
+
+        pieChart.setUsePercentValues(true);
+        setUpPieChart();
+
         return root;
     }
+
+    private void setUpPieChart() {
+        ArrayList<Entry> yvalues = new ArrayList<Entry>();
+        yvalues.add(new Entry(8f, 0));
+        yvalues.add(new Entry(15f, 1));
+
+
+        PieDataSet dataSet = new PieDataSet(yvalues, "Attendance Results");
+        ArrayList<String> xVals = new ArrayList<String>();
+        xVals.add("Absent");
+        xVals.add("Present");
+
+
+        PieData data = new PieData(xVals, dataSet);
+        // In Percentage term
+        data.setValueFormatter(new PercentFormatter());
+
+        pieChart.setData(data);
+        pieChart.setDescription("");
+//        pieChart.setDescriptionTextSize(13f);
+
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setTransparentCircleRadius(25f);
+        pieChart.setHoleRadius(25f);
+
+        dataSet.setColors(new int[]{R.color.colorAbsent , R.color.colorPresent} , getActivity());
+        data.setValueTextSize(13f);
+        data.setValueTextColor(Color.DKGRAY);
+        pieChart.setOnChartValueSelectedListener(this);
+
+        pieChart.animateXY(1400, 1400);
+
+
+    }
+
+    @Override
+    public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
+
+        if (e == null)
+            return;
+        Log.i("VAL SELECTED",
+                "Value: " + e.getVal() + ", xIndex: " + e.getXIndex()
+                        + ", DataSet index: " + dataSetIndex);
+    }
+
+    @Override
+    public void onNothingSelected() {
+        Log.i("PieChart", "nothing selected");
+    }
+
 }
